20bbad150d7bf11a046a5ff4127d150a6211a06c
Brock Altug <brock.altug99@gmail.com>
2025-01-15 18:04:10 -0800

    Set up Webpack with PWA features, service worker, and manifest generation
    - Configured HtmlWebpackPlugin to generate HTML file and inject bundled scripts.
    - Used InjectManifest to inject custom service worker for caching and offline support.
    - Integrated WebpackPwaManifest to generate a manifest.json for PWA configuration.
    - Set up CSS and Babel loaders for handling styles and modern JavaScript.
    - Defined entry points for the main app and service worker install scripts.
    - Specified output path and filenames for bundled JavaScript files.
    - Ensured compatibility with ES6+ syntax using Babel plugins.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,49 +3,49 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// workbox plugins for a service worker and manifest file.
-// CSS loaders and babel
+// Use workbox plugins for adding service worker functionality and generating the PWA manifest file.
+// Set up CSS loaders and Babel for ES6 compatibility.
 
 module.exports = () => {
   return {
-    mode: "development",
+    mode: "development", // Set the build mode to development for easier debugging.
     entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
+      main: "./src/js/index.js", // Entry point for the main application script.
+      install: "./src/js/install.js", // Entry point for the service worker install script.
     },
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: "[name].bundle.js", // Define the output file name based on entry points.
+      path: path.resolve(__dirname, "dist"), // Output path for bundled files.
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles.
+      // HtmlWebpackPlugin automatically generates an HTML file and injects the JavaScript bundles.
       new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "Text Editor",
+        template: "./index.html", // Template HTML file to use for generation.
+        title: "Text Editor", // Sets the title for the generated HTML page.
       }),
 
-      // Injects our custom service worker
+      // Inject custom service worker into the build.
       new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
+        swSrc: "./src-sw.js", // Path to the custom service worker.
+        swDest: "src-sw.js", // Output path for the generated service worker file.
       }),
 
-      // Creates a manifest.json file.
+      // WebpackPwaManifest generates the manifest.json for PWA configuration.
       new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: "Text Editor",
-        short_name: "textedit",
-        description: "Snip and save, even when offline!",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
-        start_url: "./",
-        publicPath: "./",
+        fingerprints: false, // Disable fingerprints for the icons in the manifest.
+        inject: true, // Inject the generated manifest into the HTML file.
+        name: "Text Editor", // Full name of the PWA.
+        short_name: "textedit", // Short name for the PWA, displayed on the home screen.
+        description: "Snip and save, even when offline!", // Description of the PWA's functionality.
+        background_color: "#225ca3", // Background color for the PWA.
+        theme_color: "#225ca3", // Theme color for the PWA, typically used in the address bar.
+        start_url: "./", // Start URL for the PWA.
+        publicPath: "./", // Public path for the PWA assets.
         icons: [
           {
-            src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
+            src: path.resolve("src/images/logo.png"), // Path to the icon image.
+            sizes: [96, 128, 192, 256, 384, 512], // Sizes of the icon for different device resolutions.
+            destination: path.join("assets", "icons"), // Destination folder for icons.
           },
         ],
       }),
@@ -54,20 +54,19 @@ module.exports = () => {
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          test: /\.css$/i, // Apply loaders for CSS files.
+          use: ["style-loader", "css-loader"], // Use style-loader and css-loader to process CSS files.
         },
         {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
+          test: /\.m?js$/, // Apply Babel loader for JavaScript files.
+          exclude: /node_modules/, // Exclude node_modules from being processed.
           use: {
-            loader: "babel-loader",
+            loader: "babel-loader", // Use Babel to transpile ES6+ code.
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ["@babel/preset-env"], // Preset to transpile modern JavaScript.
               plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
+                "@babel/plugin-proposal-object-rest-spread", // Plugin for object spread syntax.
+                "@babel/transform-runtime", // Plugin to optimize helper functions.
               ],
             },
           },
